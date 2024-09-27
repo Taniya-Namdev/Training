d074ae018eb887428474f380472cafdeb2f210b3
Taniya-Namdev <taniya.namdev@dianapps.com>
2024-09-27 15:18:54 +0530

    Variables commit

---
+++ Introduction/variables.py
@@ -0,0 +1,17 @@
+# Variable - It is a sybolic name that references or point to an object assingned to a variable .
+# Python is dynamically typed , there is no need of defined data type of variable during declaration .
+x = 10
+y = 'Hello'
+print(type(x),type(y))
+
+# Multiple Assignments
+a,b,c = 1,2,3
+print(a)
+print(b)
+print(c)
+
+#Swapping Allowed
+a,b = b,a
+print(a)
+print(b)
+
