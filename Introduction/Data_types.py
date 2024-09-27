70530e2645289b5c83966ce1a0f8b8a4e02cdc48
Taniya-Namdev <taniya.namdev@dianapps.com>
2024-09-27 16:40:29 +0530

    Datatype_commit

---
+++ Introduction/Data_types.py
@@ -0,0 +1,30 @@
+# # Data types in python
+# Numeric - int , float , complex
+# Sequence - str , list , tuple , range
+# Mapping - dict
+# Set - set , frozen set
+# Boolean - bool(True, False)
+# Binary - bytes , bytearray , memoryview
+# None - Represents the absence of a value
+
+x = 10
+y = 3.14
+z = 1 + 2j
+
+text = "Hello"
+numbers = [1,2,3,4]
+coordinates = (10.0,20.0)
+r = range(5)
+
+person = {"Name": "Aditya", "Age": 25}
+
+uni_numbers = {1,2,3}
+immutable_set = frozenset({1,2,3,4})
+
+is_active = True
+
+byte_data = b'hello'
+mutable_byte_data = bytearray(5)
+memory_view = memoryview(byte_data)
+
+nothing = None
