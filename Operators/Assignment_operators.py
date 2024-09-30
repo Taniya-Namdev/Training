724f40c6639403390ece7fdd321077d1d463043f
Taniya-Namdev <taniya.namdev@dianapps.com>
2024-09-30 10:19:00 +0530

    Assignment operators commit

---
+++ Operators/Assignment_operators.py
@@ -0,0 +1,40 @@
+# These operators are used to assign values to variables. They also can perform opereations on variables and assign the result back to the variable.
+
+# Assignment (=)
+a = 5
+print(a)
+
+# Addition Assignment (+=)
+a=5
+a+= 5
+print(a)
+
+# Subtraction Assignment (-=)
+a = 5 
+a-=2
+print(a)
+
+# Multiplication Assignment (*=)
+a=5
+a*=2
+print(a)
+
+# Division Assignment (/=)
+a=5
+a/=2
+
+# Modulus Assignment (%=)
+a = 10
+a%=3
+print(a)
+
+# Exponentiation Assignment(**)
+a=2
+a**=3
+print(a)
+# Floor Division Assignment(//=)
+a=10
+a//=3
+print(a)
+
+# Assignment operator can use with bitwise operators to.
