05223c8592687de287697348a2bf89eda4878e17
Taniya-Namdev <taniya.namdev@dianapps.com>
2024-09-30 09:23:44 +0530

    arithmetic operators commit

---
+++ Operators/Arth_operators.py
@@ -0,0 +1,14 @@
+# Arithmetic operators are used to perform basic mathematical oprations.
+# Addition , Subtraction , Multiplication , Division , Modulus , Exponentiation , Floor Division
+a = 5
+b = 2
+result1 = a+b
+result2 = a-b
+result3 = a*b
+result4 = 5/2
+result5 = 5%2
+result6 = 5**2
+result7 = 5//2
+
+print(result1,result2,result3,result4,result5,result6,result7)
+
