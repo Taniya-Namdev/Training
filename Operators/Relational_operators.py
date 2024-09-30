5ab76e8936b9be710bf6dd2309b750dea9466180
Taniya-Namdev <taniya.namdev@dianapps.com>
2024-09-30 09:34:14 +0530

    Relational operators commit

---
+++ Operators/Relational_operators.py
@@ -0,0 +1,18 @@
+# Relational operators in python are used to compare two values. 
+# They return Boolean value .
+# 1. Equal to (==)
+# 2. Not equal to (!=)
+# 3. Greater than (>) and Less than (<)
+# 4. Greater than equal to (>=)
+# 5. Less than equal to (<=)
+# they are commanly used for decisioc making
+
+a = 5
+b = 3
+c = 5
+result1 = ( a==c)
+result2 = (a!=b)
+result3 = (a>b)
+result4 = (b<a)
+result5 = (a>=c)
+print(result1,result2,result3,result4,result5)
