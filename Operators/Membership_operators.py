65937909adadc1122829d26adbea0f126eecf3bb
Taniya-Namdev <taniya.namdev@dianapps.com>
2024-09-30 10:28:38 +0530

    Membership operators commit

---
+++ Operators/Membership_operators.py
@@ -0,0 +1,9 @@
+# Membership operators used to test whether a value is a member of a sequence, such as string , list , tuple or dictionary.
+# in : Returns True if the specified value is found in sequence .
+a = [1,2,4,3,5]
+result = 3 in a
+print(result)
+
+# not in : Returns True if the specified value is not found in sequence
+result2 = 6 not in a
+print(result2)
