ff2718de66709937e88516b1ce9abd59bfed91ff
Taniya-Namdev <taniya.namdev@dianapps.com>
2024-09-30 16:28:00 +0530

    Practice question

---
+++ For_Loop/Practice_ques.py
@@ -0,0 +1,16 @@
+# Longest Substring Without Repeating Characters:
+
+def longest_substring(s):
+    char_set = set()
+    left = 0
+    max_length = 0
+    for right in range (len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left+=1
+        char_set.add(s[right])
+        max_length = max(max_length,right-left+1)
+    return max_length
+
+s = "abcabcbb"
+print(longest_substring(s))
