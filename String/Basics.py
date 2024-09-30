514c15e43f832701bca56ae22bc3795f1614583f
Taniya-Namdev <taniya.namdev@dianapps.com>
2024-09-30 13:31:14 +0530

    Basics of String

---
+++ String/Basics.py
@@ -0,0 +1,24 @@
+# A string is a sequence of characters used to represent text. In most programming languages, strings are treated as a data type and can be manipulated using various operations.
+
+# Concatenation :
+str1 = "Hello"
+str2 = "World"
+result = str1 + " " + str2
+
+# Substring Extraction :
+str = "Hello World"
+substr = str[0:5]
+
+# Length:
+str = "Hello"
+length = len(str)
+
+# Comparison :
+str1 = "Hello"
+str2 = "World"
+is_equal = str1 ==str2
+print(is_equal)
+
+# Searching :
+str = "Hello World"
+position = str.find("World")
